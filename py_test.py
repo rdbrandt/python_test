eb6c83d0284b189ef2abf111bcf9ba45d1a5e59d
rdbrandt <robert.d.brandt@gmail.com>
2014-07-30 14:24:02 -0500

    master branch - update 02

--- py_test.py
+++ py_test.py
@@ -1,6 +1,6 @@
 data = [1,2,3,4,5]
 
-print 'This change made in "branch 01"'
+print 'Try changing the same line that was changed in "branch 02"'
 
 for i, item in enumerate(data):
     print 'index: ' + str(i) + ',', 'value: ' + str(item)
@@ -10,6 +10,8 @@ print 'number of items in data: ' + str(len(data)) + '\n'
     
 data.append(6)
 
+
+print 'plus another change from "master"'
 print 'last element in data: ' + str(data[-1])
 
 print 'This was done in "master" branch'
